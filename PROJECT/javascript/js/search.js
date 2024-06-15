3995d5626591b2be9d7a6992c515d4966912b530
trungnguyenngoc123 <137521550+trungnguyenngoc123@users.noreply.github.com>
2024-06-15 15:58:52 +0700

    Update laptop

--- PROJECT/javascript/js/search.js
+++ PROJECT/javascript/js/search.js
@@ -1,5 +1,5 @@
 //slider
-let slideIndex = [1,1];
+let slideIndex = [1, 1];
 let slideId = ["mySlides1"];
 showSlides(1, 0);
 
@@ -10,12 +10,18 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
     let i;
     let x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) {slideIndex[no] = 1}
-    if (n < 1) {slideIndex[no] = x.length}
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > x.length) { slideIndex[no] = 1 }
+    if (n < 1) { slideIndex[no] = x.length }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    x[slideIndex[no]-1].style.display = "block";
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+    x[slideIndex[no] - 1].style.display = "block";
+    dots[slideIndex[no] - 1].className += " active-dot"; // Add the active-dot class to the current dot
 }
 
 function autoSlides() {
@@ -24,26 +30,29 @@ function autoSlides() {
     }
 }
 
-setInterval(autoSlides, 2000);
+function currentSlide(n, no) {
+    showSlides(slideIndex[no] = n, no);
+}
 
+setInterval(autoSlides, 3000);
 // address form
 const addressbtn = document.querySelector('#address-form');
 const addressclose = document.querySelector('#address-close');
 // console.log(addressbtn);
-addressbtn.addEventListener("click", function(){//add event
+addressbtn.addEventListener("click", function () {//add event
     document.querySelector('.address-form').style.display = 'flex';
 })
-addressclose.addEventListener("click", function(){//add event
+addressclose.addEventListener("click", function () {//add event
     document.querySelector('.address-form').style.display = 'none';
 })
 //login form
 const loginbtn = document.querySelector('#login-form');
 const loginclose = document.querySelector('#login-close');
 // console.log(addressbtn);
-loginbtn.addEventListener("click", function(){//add event
+loginbtn.addEventListener("click", function () {//add event
     document.querySelector('.login-form').style.display = 'flex';
 })
-loginclose.addEventListener("click", function(){//add event
+loginclose.addEventListener("click", function () {//add event
     document.querySelector('.login-form').style.display = 'none';
 })
 
@@ -51,20 +60,20 @@ loginclose.addEventListener("click", function(){//add event
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 //nút xem thêm
 function toggleContent() {
@@ -78,13 +87,24 @@ function toggleContent() {
         showMoreBtn.textContent = 'Xem thêm';
     }
 }
+function toggleContent() {
+    var moreContent = document.querySelector('.more-content1');
+    var showMoreBtn = document.querySelector('.show-more-btn1');
+    if (moreContent.style.display === 'none') {
+        moreContent.style.display = 'inline';
+        showMoreBtn.textContent = 'Thu gọn';
+    } else {
+        moreContent.style.display = 'none';
+        showMoreBtn.textContent = 'Xem thêm sản phẩm';
+    }
+}
 // footer
-$(document).ready(function() {
+$(document).ready(function () {
     $(".social-button").hover(
-        function() {
+        function () {
             $(this).find('i').css("transform", "rotate(360deg)");
-        }, 
-        function() {
+        },
+        function () {
             $(this).find('i').css("transform", "rotate(0deg)");
         }
     );
